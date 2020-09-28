0c91042c963f18b74081f50ed3782dd1555de8b1
Angel Randall <drivenbysuccess21@gmail.com>
2020-09-28 12:32:46 -0500

    removed contact info from bio

--- src/Components/About.js
+++ src/Components/About.js
@@ -27,13 +27,7 @@ class About extends Component {
             <p>{bio}</p>
             <div className="row">
                <div className="columns contact-details">
-                  <h2>Contact Details</h2>
-                  <p className="address">
-						   <span>{name}</span><br />
-						         {city} {state}, {zip}
-						   <span>{phone}</span><br />
-                     <span>{email}</span>
-					   </p>
+                  
                </div>
                {/* <div className="columns download">
                   <p>
